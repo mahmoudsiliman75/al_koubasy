c805ec9b79831b9e4ce36d12e60e21c8531d046a
MahmoudSiliman <modysiliman75@gmail.com>
2021-04-16 16:54:32 +0200

    edit pre;oader

--- layouts/TheHeader.php
+++ layouts/TheHeader.php
@@ -197,11 +197,14 @@
 <!-- END:: SMALL SCREENS MENU -->
 
 <!-- START:: PRE LOADER -->
-<!-- <div class="pre_loader">
+<div class="pre_loader">
   <img src="assets/media/logo.png">
   <div class="spinner">
-    <div class="cube1"></div>
-    <div class="cube2"></div>
+    <div class="rect1"></div>
+    <div class="rect2"></div>
+    <div class="rect3"></div>
+    <div class="rect4"></div>
+    <div class="rect5"></div>
   </div>
-</div> -->
+</div>
 <!-- END:: PRE LOADER -->
