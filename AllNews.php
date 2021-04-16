d5d472adb6aaa1b6d4bf81d26bed4582d848b0bc
MahmoudSiliman <modysiliman75@gmail.com>
2021-04-16 04:00:58 +0200

    finish all news page

--- AllNews.php
+++ AllNews.php
@@ -12,6 +12,83 @@
 </div>
 <!-- END:: HEDAER IMAGE SECTION -->
 
+<!-- START:: ALL NEWS SECTION -->
+<div class="all_news">
+  <div class="container">
+    <div class="header_wraper">
+      <h2 class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.3s"> All News </h2>
+    </div>
+
+    <div class="row justify-content-center wow fadeInLeftBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
+      <div class="col-10 col-md-4 my-3">
+        <a href="ArticleDetails.php" class="card">
+          <img src="assets/media/pic16.jpg" class="card-img-top" alt="...">
+          <div class="card-body mt-4">
+            <h4> Article Title </h4>
+            <h5> 27 - Aug - 2020 </h5>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-md-4 my-3">
+        <a href="ArticleDetails.php" class="card">
+          <img src="assets/media/pic1.jpg" class="card-img-top" alt="...">
+          <div class="card-body mt-4">
+            <h4> Article Title </h4>
+            <h5> 27 - Aug - 2020 </h5>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-md-4 my-3">
+        <a href="ArticleDetails.php" class="card">
+          <img src="assets/media/pic8.jpg" class="card-img-top" alt="...">
+          <div class="card-body mt-4">
+            <h4> Article Title </h4>
+            <h5> 27 - Aug - 2020 </h5>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-md-4 my-3">
+        <a href="ArticleDetails.php" class="card">
+          <img src="assets/media/pic15.jpg" class="card-img-top" alt="...">
+          <div class="card-body mt-4">
+            <h4> Article Title </h4>
+            <h5> 27 - Aug - 2020 </h5>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-md-4 my-3">
+        <a href="ArticleDetails.php" class="card">
+          <img src="assets/media/pic14.jpg" class="card-img-top" alt="...">
+          <div class="card-body mt-4">
+            <h4> Article Title </h4>
+            <h5> 27 - Aug - 2020 </h5>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-md-4 my-3">
+        <a href="ArticleDetails.php" class="card">
+          <img src="assets/media/pic2.jpg" class="card-img-top" alt="...">
+          <div class="card-body mt-4">
+            <h4> Article Title </h4>
+            <h5> 27 - Aug - 2020 </h5>
+          </div>
+        </a>
+      </div>
+
+      <div class="btn_wraper mt-3">
+        <a href="#"> Load More </a>
+      </div>
+
+    </div>
+  </div>
+</div>
+<!-- END:: ALL NEWS SECTION -->
+
 <!-- START:: INCLUDING THE FOOTER -->
 <?php include('layouts/TheFooter.php')?>
 <!-- END:: INCLUDING THE FOOTER -->
