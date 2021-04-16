8dc266f119d5a49e11c3c13802578b4b7b1fc1dd
MahmoudSiliman <modysiliman75@gmail.com>
2021-04-16 04:12:35 +0200

    finish article details page

---
+++ ArticleDetails.php
@@ -0,0 +1,175 @@
+<!-- START:: INCLUDING THE HEADER -->
+<?php include('layouts/TheHeader.php')?>
+<!-- END:: INCLUDING THE HEADER -->
+
+<!-- START:: HEDAER IMAGE SECTION -->
+<div class="header_img_wraper">
+  <img src="assets/media/pic10.jpg" alt="Intro Img">
+
+  <!-- START:: SECTION SPIKES -->
+  <section class="spikes"></section>
+  <!-- END:: SECTION SPIKES -->
+</div>
+<!-- END:: HEDAER IMAGE SECTION -->
+
+<!-- START:: ARTICLE DETAILS SECTION -->
+<div class="latest_news_sec">
+  <div class="container">
+    <div class="row justify-content-between p-2 overflow-hidden">
+      <div class="col-12 col-md-6 my-2 order-2 order-md-1 wow fadeInUpBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
+        <div class="latest_article_text_wraper">
+          <div class="header_wraper">
+            <h3> <Article></Article> Title </h3>
+            <h5> 27 - Aug - 2020 </h5>
+          </div>
+
+          <div class="text_wraper">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+            maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+            repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+            exercitationem temporibus veritatis nam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+            maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+            repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+            exercitationem temporibus veritatis nam.
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 my-2 order-1 order-md-2 wow fadeInUpBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
+        <div class="latest_article_img_wraper">
+          <img src="assets/media/pic5.jpg" alt="about us iamge">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: ARTICLE DETAILS SECTION -->
+
+<!-- START:: RELATED NEWS SLIDER -->
+<div class="latest_news_slider">
+  <div class="container">
+    <div class="header_wraper">
+      <h2 class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.3s"> Related Articles </h2>
+    </div>
+
+    <div class="row flex-column align-items-center justify-content-center mt-3 wow fadeInLeftBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
+      <div id="latest_news" class="owl-carousel owl-theme col-12 wow fadeInLeftBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
+
+        <div class="item">
+          <div class="row justify-content-between p-2 overflow-hidden">
+            <div class="col-12 col-md-6 my-2">
+              <div class="latest_article_img_wraper">
+                <img src="assets/media/pic5.jpg" alt="about us iamge">
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6 my-2">
+              <div class="latest_article_text_wraper">
+                <div class="header_wraper">
+                  <h3> Conference Title </h3>
+                  <h5> 27 - Aug - 2020 </h5>
+                </div>
+
+                <div>
+                  <div class="text_wraper">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                  </div>
+
+                  <div class="btn_wraper">
+                    <a href="ArticleDetails.php"> More Details </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row justify-content-between p-2 overflow-hidden">
+            <div class="col-12 col-md-6 my-2">
+              <div class="latest_article_img_wraper">
+                <img src="assets/media/pic15.jpg" alt="about us iamge">
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6 my-2">
+              <div class="latest_article_text_wraper">
+                <div class="header_wraper">
+                  <h3> Conference Title </h3>
+                  <h5> 27 - Aug - 2020 </h5>
+                </div>
+
+                <div>
+                  <div class="text_wraper">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                  </div>
+
+                  <div class="btn_wraper">
+                    <a href="ArticleDetails.php"> More Details </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row justify-content-between p-2 overflow-hidden">
+            <div class="col-12 col-md-6 my-2">
+              <div class="latest_article_img_wraper">
+                <img src="assets/media/pic22.jpg" alt="about us iamge">
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6 my-2">
+              <div class="latest_article_text_wraper">
+                <div class="header_wraper">
+                  <h3> Conference Title </h3>
+                  <h5> 27 - Aug - 2020 </h5>
+                </div>
+
+                <div>
+                  <div class="text_wraper">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                  </div>
+
+                  <div class="btn_wraper">
+                    <a href="ArticleDetails.php"> More Details </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: RELATED NEWS SLIDER -->
+
+<!-- START:: INCLUDING THE FOOTER -->
+<?php include('layouts/TheFooter.php')?>
+<!-- END:: INCLUDING THE FOOTER -->
