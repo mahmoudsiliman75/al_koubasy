19ec9e08bf31bba5afed881390d2b06c00e70637
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-04-19 12:19:33 +0200

    finish auth page edits

--- auth.php
+++ auth.php
@@ -3,13 +3,7 @@
 <!-- END:: INCLUDING THE HEADER -->
 
 <!-- START:: HEDAER IMAGE SECTION -->
-<div class="header_img_wraper">
-  <img src="assets/media/pic16.jpg" alt="Intro Img">
 
-  <!-- START:: SECTION SPIKES -->
-  <section class="spikes"></section>
-  <!-- END:: SECTION SPIKES -->
-</div>
 <!-- END:: HEDAER IMAGE SECTION -->
 
 <!-- START:: AUTH PAGE -->
