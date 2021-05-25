8aabc5f18f30cedb4f54b29854f0c789a01d87e4
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-05-25 17:18:38 +0200

    Add btn to event card will open a modal imgs

--- index.php
+++ index.php
@@ -594,179 +594,257 @@
 </div>
 <!-- END:: HEIGHLIGHTS SECTION -->
 
-<!-- START:: EVENTS SLIDER -->
-<div class="events_slider">
-  <div class="container">
+<div class="bg_wraper_2">
+  <!-- START:: EVENTS SLIDER -->
+  <div class="events_slider">
+    <div class="container">
+      <div class="header_wraper">
+        <h2 class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.3s"> Our Events </h2>
+      </div>
+  
+      <div class="row flex-column align-items-center justify-content-center mt-3 wow fadeInRightBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
+        <div id="events" class="owl-carousel owl-theme col-12">
+  
+          <div class="item">
+            <div class="row justify-content-between p-2">
+              <div class="col-12 col-md-6 my-2">
+                <div class="event_text_wraper">
+                  <div class="header_wraper">
+                    <h3> Conference Title </h3>
+                  </div>
+  
+                  <div class="text_wraper">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem.
+                  </div>
+  
+                  <div class="card_btn_wraper">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn" data-toggle="modal" data-target="#event">
+                      Events Photos
+                    </button>
+                  </div>
+                </div>
+              </div>
+  
+              <div class="col-12 col-md-5 my-2">
+                <div class="event_img_wraper">
+                  <img src="./assets/media/pic21.jpg" alt="about us iamge">
+  
+                  <div class="event_date">
+                    <i class="far fa-calendar-alt fa-lg"></i>
+                    <p>
+                      <span> Event Date </span>
+                      <span> 1 - Apr - 2021 </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+          <div class="item">
+            <div class="row justify-content-between p-2">
+              <div class="col-12 col-md-6 my-2">
+                <div class="event_text_wraper">
+                  <div class="header_wraper">
+                    <h3> Conference Title </h3>
+                  </div>
+  
+                  <div class="text_wraper">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem.
+                  </div>
+  
+                  <div class="card_btn_wraper">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn" data-toggle="modal" data-target="#event">
+                      Events Photos
+                    </button>
+                  </div>
+                </div>
+              </div>
+  
+              <div class="col-12 col-md-5 my-2">
+                <div class="event_img_wraper">
+                  <img src="./assets/media/pic10.jpg" alt="about us iamge">
+  
+                  <div class="event_date">
+                    <i class="far fa-calendar-alt fa-lg"></i>
+                    <p>
+                      <span> Event Date </span>
+                      <span> 1 - Apr - 2021 </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+          <div class="item">
+            <div class="row justify-content-between p-2">
+              <div class="col-12 col-md-6 my-2">
+                <div class="event_text_wraper">
+                  <div class="header_wraper">
+                    <h3> Conference Title </h3>
+                  </div>
+  
+                  <div class="text_wraper">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
+                    repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
+                    exercitationem temporibus veritatis nam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                    maiores, nulla explicabo exercitationem.
+                  </div>
+  
+                  <div class="card_btn_wraper">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn" data-toggle="modal" data-target="#event">
+                      Events Photos
+                    </button>
+                  </div>
+                </div>
+              </div>
+  
+              <div class="col-12 col-md-5 my-2">
+                <div class="event_img_wraper">
+                  <img src="./assets/media/pic15.jpg" alt="about us iamge">
+  
+                  <div class="event_date">
+                    <i class="far fa-calendar-alt fa-lg"></i>
+                    <p>
+                      <span> Event Date </span>
+                      <span> 1 - Apr - 2021 </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  
+        <!-- START:: EVENT PICS MODAL -->
+        <div class="modal fade" id="event" tabindex="-1" aria-labelledby="imgsModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title" id="exampleModalLabel">Conference Title</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div id="slider" class="flexslider">
+                  <ul class="slides">
+                    <li>
+                      <img src="assets/media/pic2.jpg" />
+                    </li>
+                    <li>
+                      <img src="assets/media/pic3.jpg" />
+                    </li>
+                    <li>
+                      <img src="assets/media/pic4.jpg" />
+                    </li>
+                    <li>
+                      <img src="assets/media/pic5.jpg" />
+                    </li>
+                    <li>
+                      <img src="assets/media/pic6.jpg" />
+                    </li>
+                    <li>
+                      <img src="assets/media/pic7.jpg" />
+                    </li>
+                  </ul>
+                </div>
+  
+                <div id="carousel" class="flexslider">
+                  <ul class="slides">
+                    <li>
+                      <img class="min_img" src="assets/media/pic2.jpg" />
+                    </li>
+                    <li>
+                      <img class="min_img" src="assets/media/pic3.jpg" />
+                    </li>
+                    <li>
+                      <img class="min_img" src="assets/media/pic4.jpg" />
+                    </li>
+                    <li>
+                      <img class="min_img" src="assets/media/pic5.jpg" />
+                    </li>
+                    <li>
+                      <img class="min_img" src="assets/media/pic6.jpg" />
+                    </li>
+                    <li>
+                      <img class="min_img" src="assets/media/pic7.jpg" />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- END:: EVENT PICS MODAL -->
+  
+        <div class="btn_wraper">
+          <a href="AllEvents.php"> View More </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END:: EVENTS SLIDER -->
+  
+  <!-- START:: PRODUCTS CATS SECTION -->
+  <div class="products_cats">
     <div class="header_wraper">
-      <h2 class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.3s"> Our Events </h2>
+      <h2 class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.9s"> Products Categories </h2>
     </div>
-
-    <div class="row flex-column align-items-center justify-content-center mt-3 wow fadeInRightBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
-      <div id="events" class="owl-carousel owl-theme col-12">
-
-        <div class="item">
-          <div class="row justify-content-between p-2">
-            <div class="col-12 col-md-6 my-2">
-              <div class="event_text_wraper">
-                <div class="header_wraper">
-                  <h3> Conference Title </h3>
-                </div>
-
-                <div class="text_wraper">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
-                  maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
-                  repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
-                  exercitationem temporibus veritatis nam.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
-                  maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
-                  repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
-                  exercitationem temporibus veritatis nam.
-                </div>
-              </div>
-            </div>
-
-            <div class="col-12 col-md-5 my-2">
-              <div class="event_img_wraper">
-                <img src="./assets/media/pic21.jpg" alt="about us iamge">
-
-                <div class="event_date">
-                  <i class="far fa-calendar-alt fa-lg"></i>
-                  <p>
-                    <span> Event Date </span>
-                    <span> 1 - Apr - 2021 </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+  
+    <div class="container">
+      <div class="row cats_wraper justify-content-center mt-3 overflow-hidden">
+        <div class="col-6 col-md-4 my-3 wow fadeInDownBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
+          <a href="AllProducts.php">
+            <img src="assets/media/pic4.jpg" alt="Why Section Icon 1">
+            <h5> 
+              Fresh Fruits
+            </h5>
+            <i class="fas fa-angle-double-right"></i>
+          </a>
         </div>
-
-        <div class="item">
-          <div class="row justify-content-between p-2">
-            <div class="col-12 col-md-6 my-2">
-              <div class="event_text_wraper">
-                <div class="header_wraper">
-                  <h3> Conference Title </h3>
-                </div>
-
-                <div class="text_wraper">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
-                  maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
-                  repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
-                  exercitationem temporibus veritatis nam.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
-                  maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
-                  repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
-                  exercitationem temporibus veritatis nam.
-                </div>
-              </div>
-            </div>
-
-            <div class="col-12 col-md-5 my-2">
-              <div class="event_img_wraper">
-                <img src="./assets/media/pic21.jpg" alt="about us iamge">
-
-                <div class="event_date">
-                  <i class="far fa-calendar-alt fa-lg"></i>
-                  <p>
-                    <span> Event Date </span>
-                    <span> 1 - Apr - 2021 </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+  
+        <div class="col-6 col-md-4 my-3 wow fadeInUpBig" data-wow-duration="1.5s" data-wow-delay="0.6s">
+          <a href="AllProducts.php">
+            <img src="assets/media/pic6.jpg" alt="Why Section Icon 1">
+            <h5> 
+              Fresh Vegetables
+            </h5>
+            <i class="fas fa-angle-double-right"></i>
+          </a>
         </div>
-
-        <div class="item">
-          <div class="row justify-content-between p-2">
-            <div class="col-12 col-md-6 my-2">
-              <div class="event_text_wraper">
-                <div class="header_wraper">
-                  <h3> Conference Title </h3>
-                </div>
-
-                <div class="text_wraper">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
-                  maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
-                  repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
-                  exercitationem temporibus veritatis nam.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
-                  maiores, nulla explicabo exercitationem dolorum quas veniam aut ex.
-                  repudiandae facere nostrum soluta. Dicta explicabo corrupti totam
-                  exercitationem temporibus veritatis nam.
-                </div>
-              </div>
-            </div>
-
-            <div class="col-12 col-md-5 my-2">
-              <div class="event_img_wraper">
-                <img src="./assets/media/pic21.jpg" alt="about us iamge">
-
-                <div class="event_date">
-                  <i class="far fa-calendar-alt fa-lg"></i>
-                  <p>
-                    <span> Event Date </span>
-                    <span> 1 - Apr - 2021 </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+  
+        <div class="col-6 col-md-4 my-3 wow fadeInDownBig" data-wow-duration="1.5s" data-wow-delay="0.9s">
+          <a href="AllProducts.php">
+            <img src="assets/media/pic1.jpg" alt="Why Section Icon 1">
+            <h5> 
+              Juices
+            </h5>
+            <i class="fas fa-angle-double-right"></i>
+          </a>
         </div>
-
-      </div>
-
-      <div class="btn_wraper">
-        <a href="AllEvents.php"> View More </a>
+  
       </div>
     </div>
   </div>
+  <!-- END:: PRODUCTS CATS SECTION -->
 </div>
-<!-- END:: EVENTS SLIDER -->
-
-<!-- START:: PRODUCTS CATS SECTION -->
-<div class="products_cats">
-  <div class="header_wraper">
-    <h2 class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.9s"> Products Categories </h2>
-  </div>
-
-  <div class="container">
-    <div class="row cats_wraper justify-content-center mt-3 overflow-hidden">
-      <div class="col-6 col-md-4 my-3 wow fadeInDownBig" data-wow-duration="1.5s" data-wow-delay="0.3s">
-        <a href="AllProducts.php">
-          <img src="assets/media/pic4.jpg" alt="Why Section Icon 1">
-          <h5> 
-            Fresh Fruits
-          </h5>
-          <i class="fas fa-angle-double-right"></i>
-        </a>
-      </div>
-
-      <div class="col-6 col-md-4 my-3 wow fadeInUpBig" data-wow-duration="1.5s" data-wow-delay="0.6s">
-        <a href="AllProducts.php">
-          <img src="assets/media/pic6.jpg" alt="Why Section Icon 1">
-          <h5> 
-            Fresh Vegetables
-          </h5>
-          <i class="fas fa-angle-double-right"></i>
-        </a>
-      </div>
-
-      <div class="col-6 col-md-4 my-3 wow fadeInDownBig" data-wow-duration="1.5s" data-wow-delay="0.9s">
-        <a href="AllProducts.php">
-          <img src="assets/media/pic1.jpg" alt="Why Section Icon 1">
-          <h5> 
-            Juices
-          </h5>
-          <i class="fas fa-angle-double-right"></i>
-        </a>
-      </div>
-
-    </div>
-  </div>
-</div>
-<!-- END:: PRODUCTS CATS SECTION -->
 
 <!-- START::LATEST NEWS SECTION -->
 <div class="latest_news_sec">
